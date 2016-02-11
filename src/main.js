dcd266ad4230ce4a4218212026dbb1320f0e9c7d
Vasili Kauryha <vkovrigo@gmail.com>
2016-02-11 17:34:26 +0300

    Add dragability for new react via transform

--- src/main.js
+++ src/main.js
@@ -37,15 +37,29 @@
 
     // Creare rect inside tag `g` and than add `foreignObject` with some html.
 
+    var newDrag = d3.behavior.drag()
+        .origin(function(d) { return d; })
+        .on('drag', newDragmove);
+
     var group = svg.append('g')
         .data([{x: width / 10, y: height / 10}])
         .attr('class', 'block')
         .attr('transform', function(d) {
             return 'translate(' + [ d.x, d.y ] + ')';
-        });
+        })
+        .call(newDrag);
 
     var react = group.append('rect')
         .attr('height', height)
         .attr('width', width);
+
+    function newDragmove(d) {
+        d.x += d3.event.dx;
+        d.y += d3.event.dy;
+
+        d3.select(this).attr("transform", function(d,i){
+            return "translate(" + [ d.x, d.y ] + ")";
+        });
+    }
 
 }(d3));
