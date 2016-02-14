c0483fabb53e4a1cc63a1a5130f578ac7566f5f7
Vasili Kauryha <vkovrigo@gmail.com>
2016-02-14 20:17:11 +0300

    Move all related to main graph to separate file

--- src/app.js
+++ src/app.js
@@ -4,52 +4,12 @@
     var canvasWidth = 600,
         canvasHeight = 600;
 
-    var drag = d3.behavior.drag()
-        .origin(function(d) { return d })
-        .on('dragstart', dragstart)
-        .on('drag', dragmove);
-
-    function dragstart() {
-        d3.event.sourceEvent.stopPropagation();
-    }
-
-    function dragmove(d) {
-        d.x += d3.event.dx;
-        d.y += d3.event.dy;
-
-        d3.select(this).attr("transform", function(d){
-            return "translate(" + [ d.x, d.y ] + ")";
-        });
-    }
-
-    var zoom = d3.behavior.zoom()
-        .scaleExtent([1, 10])
-        .on('zoom', zoomed);
-
-    function zoomed() {
-        d3.select(this).attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-    }
-
     var svg = d3.select('body').append('svg')
         .attr('width', canvasWidth)
-        .attr('height', canvasHeight)
+        .attr('height', canvasHeight);
 
-    var container = svg.append('g')
-        .attr("transform", "translate(-5,-5)")
-        .call(zoom);
-
-    var block1 = new app.Block({
-        container: container,
-        x: 20,
-        y: 20,
-        dragBehavior: drag
-    });
-
-    var block2 = new app.Block({
-        container: container,
-        x: 20,
-        y: 200,
-        dragBehavior: drag
-    });
+    var graph = new app.Graph({
+        svg: svg
+    })
 
 }(app, d3));
---
+++ src/graph.js
@@ -0,0 +1,72 @@
+(function(app, d3) {
+    'use strict';
+
+    /**
+     * { function_description }
+     *
+     * @class
+     * @param      {<type>}  options  { description }
+     */
+    var Graph = function(options) {
+        this.zoom = getZoom();
+
+        this.container = options.svg.append('g')
+            .attr('class', 'graph-container')
+            .attr("transform", "translate(-5,-5)")
+            .call(this.zoom);
+
+        this.drag = getDrag();
+
+        this.blocks = [
+            new app.Block({
+                container: this.container,
+                x: 20,
+                y: 20,
+                dragBehavior: this.drag
+            }),
+            new app.Block({
+                container: this.container,
+                x: 20,
+                y: 200,
+                dragBehavior: this.drag
+            })
+        ];
+    };
+
+
+    function getZoom() {
+        var zoom = d3.behavior.zoom()
+            .scaleExtent([1, 10])
+            .on('zoom', zoomed);
+
+        function zoomed() {
+            d3.select(this).attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+        }
+
+        return zoom;
+    };
+
+    function getDrag() {
+        var drag = d3.behavior.drag()
+            .origin(function(d) { return d })
+            .on('dragstart', dragstart)
+            .on('drag', dragmove);
+
+        function dragstart() {
+            d3.event.sourceEvent.stopPropagation();
+        }
+
+        function dragmove(d) {
+            d.x += d3.event.dx;
+            d.y += d3.event.dy;
+
+            d3.select(this).attr("transform", function(d){
+                return "translate(" + [ d.x, d.y ] + ")";
+            });
+        }
+
+        return drag;
+    }
+
+    app.Graph = Graph;
+}(app, d3));
