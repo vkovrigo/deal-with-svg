20ec7ebdc668d8fb6049eb47736dade36631f4e8
Vasili Kauryha <vkovrigo@gmail.com>
2016-02-16 10:00:07 +0300

    Move initial data for graph to app js

--- src/app.js
+++ src/app.js
@@ -4,12 +4,50 @@
     var canvasWidth = 600,
         canvasHeight = 600;
 
+    // Initial data
+    var vertices = [
+            {
+                id: 0,
+                coordinates: {
+                    x: 20,
+                    y: 20,
+                }
+            },
+            {
+                id: 1,
+                coordinates: {
+                    x: 20,
+                    y: 200,
+                }
+            },
+            {
+                id: 2,
+                coordinates: {
+                    x: 300,
+                    y: 200,
+                }
+            }
+        ],
+        edges = [
+            {
+                source: vertices[0],
+                target: vertices[1]
+            },
+            {
+                source: vertices[0],
+                target: vertices[2]
+            }
+        ];
+
+
     var svg = d3.select('body').append('svg')
         .attr('width', canvasWidth)
         .attr('height', canvasHeight);
 
     var graph = new app.Graph({
-        svg: svg
+        svg: svg,
+        vertices: vertices,
+        edges: edges
     })
 
 }(app, d3));
