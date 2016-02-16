7651b57970dffd03e3ba267631b5aa5fa07831f9
Vasili Kauryha <vkovrigo@gmail.com>
2016-02-16 10:10:42 +0300

    Rewrite block coordinates to object because it should be same object like in graph vertices

--- src/block.js
+++ src/block.js
@@ -8,8 +8,9 @@
      * @param {Object} options - The initial property;
      * @param {Object} options.container - The d3 elements that should contains this block;
      * @param {Object} options.payload - The information for display on the current block;
-     * @param {number} options.x - The x position coordinate;
-     * @param {number} options.y - The y position coordinate;
+     * @param {Object} options.coordinates - The initial position for block;
+     * @param {number} options.coordinates.x - The x position coordinate;
+     * @param {number} options.coordinates.y - The y position coordinate;
      * @param {function} options.dragBehavior - The d3 drag behavior;
      */
     var Block = function(options) {
@@ -17,11 +18,10 @@
         var width = 200,
             height = 100;
 
-        this.group = options.container.append('g')
-            .data([{
-                x: options.x,
-                y: options.y
-            }])
+        this.id = options.id || idGenerator();
+
+        this.group = options.container
+            .data([options.coordinates])
             .attr('fill-opacity', 0.2)
             .attr('class', 'block')
             .attr('transform', function(d) {
