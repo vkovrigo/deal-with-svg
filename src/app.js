c1c766f215daed071c0872fda701455b5e51f9f9
Vasili Kauryha <vkovrigo@gmail.com>
2016-02-19 16:30:16 +0300

    Extend initial graph data for draw different types of blocks

--- src/app.js
+++ src/app.js
@@ -8,23 +8,50 @@
     var vertices = [
             {
                 id: 1,
+                type: app.Block.type.start,
                 coordinates: {
-                    x: 120,
-                    y: 70,
+                    x: 300,
+                    y: 60,
                 }
             },
             {
                 id: 2,
+                type: app.Block.type.say,
                 coordinates: {
-                    x: 120,
-                    y: 200,
+                    x: 300,
+                    y: 180,
                 }
             },
             {
                 id: 3,
+                type: app.Block.type.input,
+                coordinates: {
+                    x: 300,
+                    y: 300,
+                }
+            },
+            {
+                id: 4,
+                type: app.Block.type.inputValue,
+                coordinates: {
+                    x: 250,
+                    y: 375,
+                }
+            },
+            {
+                id: 5,
+                type: app.Block.type.inputError,
                 coordinates: {
                     x: 350,
-                    y: 350,
+                    y: 375,
+                }
+            },
+            {
+                id: 6,
+                type: app.Block.type.finish,
+                coordinates: {
+                    x: 300,
+                    y: 500,
                 }
             }
         ],
@@ -34,9 +61,25 @@
                 target: vertices[1].id
             },
             {
-                source: vertices[0].id,
+                source: vertices[1].id,
                 target: vertices[2].id
-            }
+            },
+            {
+                source: vertices[2].id,
+                target: vertices[3].id
+            },
+            {
+                source: vertices[2].id,
+                target: vertices[4].id
+            },
+            {
+                source: vertices[3].id,
+                target: vertices[5].id
+            },
+            {
+                source: vertices[4].id,
+                target: vertices[5].id
+            },
         ];
 
 
