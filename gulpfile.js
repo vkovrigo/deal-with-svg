e39f0813403d6cea917db610a901182592974238
Vasili Kauryha <vkovrigo@gmail.com>
2016-03-03 18:56:20 +0300

    Setup less compilation and auto reload on less files change

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    less = require('gulp-less'),
+    path = require('path'),
+    concat = require('gulp-concat');
 
 gulp.task('connect', function() {
     connect.server({
@@ -17,6 +20,17 @@ gulp.task('js', function () {
 
 gulp.task('watch', function () {
     gulp.watch(['./src/*.js'], ['js']);
+    gulp.watch(['./src/less/**/*.less'], ['less']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch', 'less']);
+
+gulp.task('less', function () {
+  return gulp.src('./src/less/**/*.less')
+    .pipe(less({
+        paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(concat('graph.css'))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(connect.reload());
+});
