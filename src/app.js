f5a7557396f18df252aac2e02c8e2c042e397aa6
Vasili Kauryha <vkovrigo@gmail.com>
2016-03-04 00:01:13 +0300

    Update initial data w/ more real dialog

--- src/app.js
+++ src/app.js
@@ -4,78 +4,208 @@
     var canvasWidth = 1024,
         canvasHeight = 768;
 
-    // Initial data
     var vertices = [
             {
                 id: 1,
                 type: app.Block.type.start,
                 coordinates: {
-                    x: 300,
-                    y: 60,
+                    x: 90,
+                    y: 40,
                 }
             },
             {
                 id: 2,
-                type: app.Block.type.say,
+                type: app.Block.type.input,
                 coordinates: {
-                    x: 300,
-                    y: 180,
+                    x: 150,
+                    y: 250,
                 },
                 payload: [
                     {
                         id: 0,
-                        message: 'Hello bro! Say hello me!'
-                    }
+                        text: '1',
+                        error: false
+                    },
+                    {
+                        id: 1,
+                        text: '2',
+                        error: false
+                    },
+                    {
+                        id: 2,
+                        text: '0',
+                        error: false
+                    },
+                    {
+                        id: 3,
+                        text: '?',
+                        error: true
+                    },
                 ]
             },
             {
                 id: 3,
-                type: app.Block.type.input,
+                type: app.Block.type.converter,
                 coordinates: {
-                    x: 300,
-                    y: 300,
+                    x: 120,
+                    y: 400
                 },
                 payload: [
                     {
-                        id: 1,
-                        text: '?',
-                        error: true
+                        id: 0,
+                        from: '1',
+                        to: 'Paolo Meneguzzi'
                     },
                     {
-                        id: 0,
-                        text: 'All user input',
-                        error: false
+                        id: 1,
+                        from: '2',
+                        to: 'Fernando Corena'
                     }
                 ]
             },
             {
                 id: 4,
+                type: app.Block.type.say,
+                coordinates: {
+                    x: 150,
+                    y: 600,
+                },
+                payload: [
+                    {
+                        id: 0,
+                        message: 'Your vote was %input%. Thank you for voting.'
+                    }
+                ]
+            },
+            {
+                id: 5,
+                type: app.Block.type.say,
+                coordinates: {
+                    x: 400,
+                    y: 511,
+                },
+                payload: [
+                    {
+                        id: 0,
+                        message: 'You are not sure. Thank you for voting.'
+                    }
+                ]
+            },
+            {
+                id: 6,
+                type: app.Block.type.say,
+                coordinates: {
+                    x: 400,
+                    y: 100,
+                },
+                payload: [
+                    {
+                        id: 0,
+                        message: 'Please, try again.'
+                    }
+                ]
+            },
+            {
+                id: 7,
                 type: app.Block.type.finish,
                 coordinates: {
                     x: 300,
-                    y: 500,
+                    y: 740,
                 }
             }
         ],
         edges = [
             {
-                source: { blockId: vertices[0].id, portId: 0 },
-                target: { blockId: vertices[1].id, portId: 0 }
+                "source": {
+                    "blockId": 1,
+                    "portId": 0
+                },
+                "target": {
+                    "blockId": 2,
+                    "portId": 0
+                }
             },
             {
-                source: { blockId: vertices[1].id, portId: 0 },
-                target: { blockId: vertices[2].id, portId: 0 }
+                "source": {
+                    "blockId": 2,
+                    "portId": 0
+                },
+                "target": {
+                    "blockId": 3,
+                    "portId": 0
+                }
             },
             {
-                source: { blockId: vertices[2].id, portId: 0 },
-                target: { blockId: vertices[3].id, portId: 0 }
+                "source": {
+                    "blockId": 2,
+                    "portId": 1
+                },
+                "target": {
+                    "blockId": 3,
+                    "portId": 0
+                }
             },
             {
-                source: { blockId: vertices[2].id, portId: 1 },
-                target: { blockId: vertices[3].id, portId: 0 }
+                "source": {
+                    "blockId": 2,
+                    "portId": 2
+                },
+                "target": {
+                    "blockId": 5,
+                    "portId": 0
+                }
             },
+            {
+                "source": {
+                    "blockId": 2,
+                    "portId": 3
+                },
+                "target": {
+                    "blockId": 6,
+                    "portId": 0
+                }
+            },
+            {
+                "source": {
+                    "blockId": 3,
+                    "portId": 0
+                },
+                "target": {
+                    "blockId": 4,
+                    "portId": 0
+                }
+            },
+            {
+                "source": {
+                    "blockId": 4,
+                    "portId": 0
+                },
+                "target": {
+                    "blockId": 7,
+                    "portId": 0
+                }
+            },
+            {
+                "source": {
+                    "blockId": 6,
+                    "portId": 0
+                },
+                "target": {
+                    "blockId": 2,
+                    "portId": 0
+                }
+            },
+            {
+                "source": {
+                    "blockId": 5,
+                    "portId": 0
+                },
+                "target": {
+                    "blockId": 7,
+                    "portId": 0
+                }
+            }
         ];
-
 
     var svg = d3.select('body').append('svg')
         .attr('width', canvasWidth)
